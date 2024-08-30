9367cca2883171c7234965829131366be466f5e8
Omar Abou Chaer <aboushaar.omar@gmail.com>
2024-08-30 01:59:34 -0400

    created the signin page

---
+++ QuotesPal/app/(auth)/sign-in.jsx
@@ -0,0 +1,149 @@
+import { View, Text, StyleSheet, Image, TextInput, Button } from "react-native";
+import { widthScale, heightScale, moderateScale } from "../../utils/scale";
+import React from "react";
+import CustomButton from "../../components/Button";
+
+const SignIn = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.quotesBackground}>
+        <Image
+          source={require("../../assets/images/quotesBackground.png")}
+          style={styles.quotesBackgroundImage}
+        />
+      </View>
+      <View style={styles.logoContainer}>
+        <Image
+          source={require("../../assets/logo/logo.png")}
+          style={styles.logo}
+        />
+        <Text style={styles.logoText}>QuotesPal</Text>
+      </View>
+
+      <View style={styles.formContainer}>
+        <View style={styles.emailContainer}>
+          <TextInput
+            style={styles.emailInput}
+            placeholder="EMAIL"
+            placeholderTextColor={"#667F8E"}
+          />
+        </View>
+        <View style={styles.passwordContainer}>
+          <TextInput
+            style={styles.passwordInput}
+            placeholder="PASSWORD"
+            placeholderTextColor={"#667F8E"}
+            secureTextEntry={true}
+          />
+        </View>
+
+        <View style={styles.loginButtonContainer}>
+          <CustomButton
+            onPress={() => {}}
+            title="Login"
+            customWidth={widthScale(278)}
+            customHeight={heightScale(48)}
+            fontSize={moderateScale(22)}
+          />
+        </View>
+      </View>
+
+      <View style={styles.noAccountContainer}>
+        <Text style={styles.noAccountText}>Don't have an account?</Text>
+      </View>
+
+      <View style={styles.signUpContainer}>
+        <CustomButton
+          onPress={() => {}}
+          title="Sign Up"
+          customWidth={widthScale(158)}
+          customHeight={heightScale(42)}
+          fontSize="19"
+          customStylingText={{ fontFamily: "Raleway-Regular" }}
+        />
+      </View>
+
+      <View style={styles.forgotPasswordContainer}>
+        <Text style={styles.forgotPasswordText}>forgot password?</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25485E",
+    padding: 0,
+    margin: 0,
+  },
+  logoContainer: {
+    alignItems: "center",
+  },
+  logo: {
+    width: widthScale(213),
+    height: heightScale(226),
+    marginBottom: heightScale(-50),
+    marginTop: heightScale(30),
+  },
+  logoText: {
+    fontFamily: "AnekDevanagari-Regular",
+    fontSize: moderateScale(40),
+    color: "#D9D9D9",
+  },
+  quotesBackground: {
+    position: "absolute",
+  },
+  quotesBackgroundImage: {
+    transform: [{ scale: 0.9 }],
+    opacity: 0.6,
+  },
+  formContainer: {
+    alignItems: "center",
+    marginTop: heightScale(50),
+  },
+  emailInput: {
+    height: 40,
+    borderBottomWidth: 1,
+    width: widthScale(280),
+    borderBottomColor: "white",
+    fontFamily: "Homenaje-Regular",
+    marginBottom: heightScale(46),
+    fontSize: moderateScale(20),
+    color: "white",
+  },
+  passwordInput: {
+    height: 40,
+    width: widthScale(280),
+    borderBottomWidth: 1,
+    borderBottomColor: "white",
+    fontFamily: "Homenaje-Regular",
+    fontSize: moderateScale(20),
+    color: "white",
+  },
+  loginButtonContainer: {
+    marginTop: heightScale(45),
+  },
+  noAccountContainer: {
+    alignItems: "center",
+  },
+  noAccountText: {
+    fontFamily: "Raleway-Regular",
+    fontSize: moderateScale(17),
+    color: "#FFFFFF",
+    marginTop: heightScale(105),
+  },
+  signUpContainer: {
+    alignItems: "center",
+    marginTop: heightScale(23),
+  },
+  forgotPasswordContainer: {
+    alignItems: "center",
+  },
+  forgotPasswordText: {
+    color: "#667F8E",
+    marginTop: heightScale(50),
+  },
+});
+
+export default SignIn;
--- QuotesPal/app/index.jsx
+++ QuotesPal/app/index.jsx
@@ -5,20 +5,28 @@ import {
   Image,
   StyleSheet,
   TouchableOpacity,
+  Button,
 } from "react-native";
 import { useEffect } from "react";
 import { useFonts } from "expo-font";
+import { useRouter } from "expo-router";
 import * as SplashScreen2 from "expo-splash-screen";
-import { widthScale, heightScale, moderateScale } from "../utils/scale";
-import { SafeAreaView } from 'react-native-safe-area-context';
 import React from "react";
+import { widthScale, heightScale, moderateScale } from "../utils/scale";
+import CustomButton from "../components/Button";
 
 SplashScreen2.preventAutoHideAsync();
 
 const SplashScreen = () => {
+  const router = useRouter();
+
   const [loaded, error] = useFonts({
     "Roboto-Regular": require("../assets/fonts/Roboto-Regular.ttf"),
-    "AnekDevanagari-Regular": require("../assets/fonts/AnekDevanagari-Regular.ttf")
+    "AnekDevanagari-Regular": require("../assets/fonts/AnekDevanagari-Regular.ttf"),
+    "Crushed-Regular": require("../assets/fonts/Crushed-Regular.ttf"),
+    "Homenaje-Regular": require("../assets/fonts/Homenaje-Regular.ttf"),
+    "Raleway-Bold": require("../assets/fonts/Raleway-Bold.ttf"),
+    "Raleway-Regular": require("../assets/fonts/Raleway-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -40,13 +48,15 @@ const SplashScreen = () => {
         />
         <Text style={styles.logoText}>QuotesPal</Text>
       </View>
-
       <View style={styles.rectangle} />
-
       <View style={styles.buttonContainer}>
-      <TouchableOpacity >
-        <Text style={styles.buttonText}>Continue</Text>
-      </TouchableOpacity>
+        <CustomButton
+          onPress={() => router.push("/sign-in")}
+          title="Continue"
+          customWidth={widthScale(177)}
+          customHeight={heightScale(48)}
+          fontSize={moderateScale(20)}
+        />
       </View>
     </View>
   );
@@ -59,22 +69,10 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     alignItems: "center",
     padding: 0,
-    margin: 0
+    margin: 0,
   },
   buttonContainer: {
-    backgroundColor: "#FFD700",
-    width: widthScale(177),
-    height: heightScale(48),
-    borderRadius: moderateScale(25),
-    justifyContent: "center",
-    alignItems: "center",
-    marginBottom: heightScale(170)
-    
-  },
-  buttonText: {
-    fontFamily: "Roboto-Regular",
-    fontSize: moderateScale(20),
-    color: "#25485E"
+    marginBottom: heightScale(170),
   },
   rectangle: {
     width: widthScale(430),
@@ -83,28 +81,28 @@ const styles = StyleSheet.create({
     borderRadius: moderateScale(600),
     position: "absolute",
     bottom: heightScale(381),
-    shadowColor: '#000000',
-    shadowOffset: { width: 0, height: 10 }, 
-    shadowOpacity: 0.17, 
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.17,
     shadowRadius: 15,
     // Android elevation
-    elevation: 10
+    elevation: 10,
   },
   logoContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     zIndex: 1,
     marginBottom: heightScale(250),
   },
   logo: {
     width: widthScale(274),
     height: heightScale(292),
-    marginBottom: heightScale(-60)
+    marginBottom: heightScale(-60),
   },
   logoText: {
-    fontFamily: 'AnekDevanagari-Regular',
+    fontFamily: "AnekDevanagari-Regular",
     fontSize: moderateScale(40),
-    color: "#25485E"
-  }
+    color: "#25485E",
+  },
 });
 
 export default SplashScreen;
