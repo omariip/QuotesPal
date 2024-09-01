24b4babcd92d32e4b677d0b4069cd47fc32e507b
Omar Abou Chaer <aboushaar.omar@gmail.com>
2024-09-01 05:24:44 -0400

    created a third tab

---
+++ QuotesPal/app/(tabs)/myquotes.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MyQuotes = () => {
+  return (
+    <View>
+      <Text>MyQuotes</Text>
+    </View>
+  );
+};
+
+export default MyQuotes;
