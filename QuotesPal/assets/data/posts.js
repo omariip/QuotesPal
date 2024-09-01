37f7ada943d62ae982c22945f3667056341ba7af
Omar Abou Chaer <aboushaar.omar@gmail.com>
2024-09-01 05:25:50 -0400

    created posts mock data

---
+++ QuotesPal/assets/data/posts.js
@@ -0,0 +1,12 @@
+const posts = [
+  {
+    author: "John Smith",
+    group: "George Orwell Lovers",
+    date: new Date(),
+    quoteAuthor: "George Orwell",
+    liked: true,
+    quote: "Perhaps one did not want to be loved so much as to be understood",
+  },
+];
+
+export default posts;
