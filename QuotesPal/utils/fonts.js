af6eae3caa012374ff12759fee06db154b07e9c3
Omar Abou Chaer <aboushaar.omar@gmail.com>
2024-09-01 05:25:10 -0400

    created a font enum

---
+++ QuotesPal/utils/fonts.js
@@ -0,0 +1,13 @@
+const fonts = {
+  Roboto_Regular: "Roboto-Regular",
+  Roboto_Medium: "Roboto-Medium",
+  Roboto_Light: "Roboto-Light",
+  AnekDevanagari_Regular: "AnekDevanagari-Regular",
+  Crushed_Regular: "Crushed-Regular",
+  Homenaje_Regular: "Homenaje-Regular",
+  Raleway_Bold: "Raleway-Bold",
+  Raleway_Regular: "Raleway-Regular",
+  Raleway_Light: "Raleway-Light",
+};
+
+export default fonts;
